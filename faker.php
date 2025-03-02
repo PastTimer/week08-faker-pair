ba92cecaf641b7b442ad97d50a60e4a567c259e5
Imee <vitoimee@gmail.com>
2025-03-02 21:15:59 +0800

    added initial faker code for populating db

--- faker.php
+++ faker.php
@@ -1,2 +1,33 @@
 <?php
+require_once 'vendor/autoload.php';
+
+$faker = Faker\Factory::create('en_PH');
+
+for ($i = 0; $i < 200; $i++) {
+    $lastName = $faker->lastName;
+    $firstName = $faker->firstName;
+    $office_id = $faker->numberBetween($min = 1, $max = 200);
+    $city = $faker->city;
+
+}
+
+for ($i = 0; $i < 50; $i++) {
+    $name = $faker->company;
+    $contactnum = $faker->phoneNumber;
+    $email = $faker->email;
+    $address = $faker->address;
+    $city = $faker->city;
+    $country = $faker->country;
+    $postal = $faker->postcode;
+}
+
+for ($i = 0; $i < 500; $i++) {
+    $employee_id = $faker->numberBetween(1, 200); 
+    $office_id = $faker->numberBetween(1, 50); 
+    $datelog = $faker->dateTimeBetween('-5 years', 'now')->format('Y-m-d H:i:s'); 
+    $action = $faker->word;
+    $remarks = $faker->words(5, true); 
+    $documentcode = $faker->uuid;
+}
+
 ?>
