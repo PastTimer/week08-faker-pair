fec566facf3de47a92ff1a5279509ce814f17dec
Imee <vitoimee@gmail.com>
2025-03-11 12:30:15 +0800

    Re-installer Faker and fixed errors on item 2

--- Additional_Item2.php
+++ Additional_Item2.php
@@ -1,54 +1,45 @@
 <?php
 require_once 'vendor/autoload.php';
-use Faker/Factory;
 
-$faker =Factory::create();
-$genres = ['Fiction', 'Mystery', 'Science Fiction',  'Fantacy',  'Romance', 'Thriller', 'Historical', 'Horror'];
+$faker = Faker\Factory::create();
+$genres = ['Fiction', 'Mystery', 'Science Fiction', 'Fantasy', 'Romance', 'Thriller', 'Historical', 'Horror'];
 
 function generateIsbn() {
-    $isbn = '';
-    for ($i=0; $i<13; $i++) {
-        $isbn .= rand(0, 9);
-    }
-    return $isbn
+    return implode('', array_map(fn() => rand(0, 9), range(1, 13)));
 }
 
 $books = [];
-for ($i=0; $i <15;$i++){
-$books = [ 'Title' => $faker->words(3, true),
-            'Author' => $faker->name,
-            'Genre' => $faker->randomElement($genres),
-            'Publication Year' => $faker->numberBetween(1900, 2024),
-            'ISBN' => generateIsbn(),
-            'Summary' => $faker->paragraph,
-        ];
-        $books[] = $book;
+for ($i = 0; $i < 15; $i++) {
+    $books[] = [ 
+        'Title' => $faker->sentence(3),
+        'Author' => $faker->name,
+        'Genre' => $faker->randomElement($genres),
+        'Publication Year' => $faker->numberBetween(1900, 2024),
+        'ISBN' => generateIsbn(),
+        'Summary' => $faker->paragraph(),
+    ];
 }
-echo "table border='1'";
-echo "<tr>";
-echo "<th>Title</th>";
-echo "<th>Author</th>";
-echo "<th>Genre</th>";
-echo "<th>Publication Year</th>";
-echo "<th>ISBN</th>";
-echo "<th>Summary</th>";
-echo "</tr>";
 
-for each ($books as $book) {
-    echo "<tr>";
-    echo "<td>";
-$book['Title'] . "</td>";
-    echo "<td>" .
-$book['Author'] . "</td>";
-echo "<td>" .
-$book['Genre'] . "</td>";
-echo "<td>" .
-$book['Publication Year'] . "</td>";
-echo "<td>" .
-$book['ISBN'] . "</td>";
-echo "<td>" .
-$book['Summary'] . "...</td>";
-echo "</tr>"
+echo "<table border='1'>";
+echo "<tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Genre</th>
+        <th>Publication Year</th>
+        <th>ISBN</th>
+        <th>Summary</th>
+      </tr>";
+
+foreach ($books as $book) {
+    echo "<tr>
+            <td>" . htmlspecialchars($book['Title']) . "</td>
+            <td>" . htmlspecialchars($book['Author']) . "</td>
+            <td>" . htmlspecialchars($book['Genre']) . "</td>
+            <td>" . htmlspecialchars($book['Publication Year']) . "</td>
+            <td>" . htmlspecialchars($book['ISBN']) . "</td>
+            <td>" . htmlspecialchars($book['Summary']) . "</td>
+          </tr>";
 }
-echo "</table>"
+
+echo "</table>";
 ?>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,7 +6,15 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit06a246fb7fd541d754d977d9b5f184ae
 {
+    public static $files = array (
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Psr\\Container\\' => 14,
+        ),
         'F' => 
         array (
             'Faker\\' => 6,
@@ -14,9 +22,13 @@ class ComposerStaticInit06a246fb7fd541d754d977d9b5f184ae
     );
 
     public static $prefixDirsPsr4 = array (
+        'Psr\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
         'Faker\\' => 
         array (
-            0 => __DIR__ . '/..' . '/fzaninotto/faker/src/Faker',
+            0 => __DIR__ . '/..' . '/fakerphp/faker/src/Faker',
         ),
     );
 
